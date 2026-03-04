5b9b2dfcd099000bda7094ecbbad1a2aae72466c
eludderboy <brasilcatinho@gmail.com>
2026-03-03 17:18:26 -0800

    Update Comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -1,13 +1,9 @@
 const mongoose = require("mongoose");
 
-const commentSchema = new mongoose.Schema(
-  {
-    post:   { type: mongoose.Schema.Types.ObjectId, ref: "Post",  required: true },
-    autor:  { type: mongoose.Schema.Types.ObjectId, ref: "User",  required: true },
-    texto:  { type: String, required: true, trim: true, maxlength: 280 },
-    likes:  [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
-  },
-  { timestamps: true }
-);
+const commentSchema = new mongoose.Schema({
+  post:   { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
+  autor:  { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  texto:  { type: String, required: true, maxlength: 500 }
+}, { timestamps: true });
 
 module.exports = mongoose.model("Comment", commentSchema);
